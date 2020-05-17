4bac5f85ad23e9ca690cce5e11e10446f69c32bc
savinanton <sandern503@gmail.com>
2020-05-17 12:49:40 +0200

    rewrote all files, until 18-th lesson,including

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+const {
+  src,
+  dest,
+  watch
+} = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
+
+
+// Static server
+function bs() {
+  serveSass();
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+  watch("./*.html").on('change', browserSync.reload);
+  watch("./sass/**/*.sass", serveSass);
+  watch("./sass/**/*.scss", serveSass);
+  watch("./js/*.js").on('change', browserSync.reload);
+};
+
+// Compile sass into CSS & auto-inject into browsers
+function serveSass() {
+  return src("./sass/**/*.sass", "./sass/**/*.scss")
+    .pipe(sass())
+    .pipe(autoprefixer({
+      cascade: false
+    }))
+    .pipe(dest("./css"))
+    .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
