03dd4e8aca2f0984ea8c9afedbc59037527d68d6
savinanton <sandern503@gmail.com>
2020-05-24 20:27:49 +0200

    changed font size, und made unvisible modal dialog after start

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
- const modal = document.querySelector(".modal");   //обратимся к окну
- const modalBtn = document.querySelectorAll("[data-toggle=modal]");
- const closeBtn = document.querySelector(".modal__close");
- const switchModal = () => {
-  modal.classList.toggle ("modal--visible");
-}
+document.addEventListener("DOMContentLoaded", function (event) {
+  const modal = document.querySelector(".modal"); //обратимся к окну
+  const modalBtn = document.querySelectorAll("[data-toggle=modal]");
+  const closeBtn = document.querySelector(".modal__close");
+  const switchModal = () => {
+    modal.classList.toggle("modal--visible");
+  };
   modalBtn.forEach((element) => {
     element.addEventListener("click", switchModal);
   });
-closeBtn.addEventListener("click", switchModal);
+  closeBtn.addEventListener("click", switchModal);
 });
+
 /*
 $(document).ready(function () {
   var modal = $(".modal"),
-      modalBtn = $("[data-toggle=modal]"),
-      closeBtn = $(".modal__close");
-   
+    modalBtn = $("[data-toggle=modal]"),
+    closeBtn = $(".modal__close");
+
   modalBtn.on("click", function () {
-  modal.toggleClass("modal--visible");
+    modal.toggleClass("modal--visible");
   });
   closeBtn.on("click", function () {
     modal.toggleClass("modal--visible");
   });
 });
-
 */
